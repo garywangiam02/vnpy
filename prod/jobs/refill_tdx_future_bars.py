2e50acf7c17aacaaf5c73617679cb7a72ea3cc31
garywangiam02 <109332409@qq.com>
2021-10-20 09:36:46 +0800

    a

--- prod/jobs/refill_tdx_future_bars.py
+++ prod/jobs/refill_tdx_future_bars.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     bar_data_folder = os.path.abspath(os.path.join(vnpy_root, 'bar_data'))
 
     # 开始日期（每年大概需要几分钟）
-    start_date = '20160101'
+    start_date = '20210101'
 
     # 创建API对象
     api_01 = TdxFutureData()
